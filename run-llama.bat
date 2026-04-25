5bc6566b7472da293b9122659aea3a56aacf0d4e
s4ndm4n33 <s4ndm4n33@gmail.com>
2026-04-24 22:43:35 -0500

    build: context limit increased to 4096; persona J. stabilized

--- run-llama.bat
+++ run-llama.bat
@@ -3,4 +3,4 @@ setlocal
 cd /d "%~dp0"
 set "STAMP=%date% %time%"
 echo [%STAMP%] Running shard-local llama CLI
-"%~dp0model-server\llama.exe" --model "%~dp0models\brain.gguf" --device none --ctx-size 1024 --threads 2 --temp 0.1 --top-p 0.85 --top-k 20 --min-p 0 --chat-template-file "%~dp0prompts\brain-chat-template.jinja" --reasoning-budget 0 --reasoning-format none --simple-io %*
+"%~dp0model-server\llama.exe" --model "%~dp0models\brain.gguf" --device none --ctx-size 4096 --threads 2 --temp 0.1 --top-p 0.85 --top-k 20 --min-p 0 --chat-template-file "%~dp0prompts\brain-chat-template.jinja" --reasoning-budget 0 --reasoning-format none --simple-io %*
